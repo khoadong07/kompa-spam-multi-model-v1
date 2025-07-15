2ee558d8424491d6695cd428d787eb65e04b9d0b
khoa.dong <khoa.dong@aurora-tech.com>
2025-07-15 23:47:23 +0700

    update

--- start_socket.sh
+++ start_socket.sh
@@ -6,5 +6,5 @@ cd socket_server
 nohup uvicorn socket_server:asgi_app --host 0.0.0.0 --port 5001 --workers 4 > ../logs/socket_server.log 2>&1 &
 
 # Save the PID
-echo $! > ../socket_server.pid
+echo $! > socket_server.pid
 echo "Inference server started with PID $(cat socket_server.pid)"
